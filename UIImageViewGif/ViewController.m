699aa90fff2ef92aee9b00f2d03fe19d98a875a2
日久才会生情1993 <840815514@qq.com>
2018-11-21 14:12:02 +0800

    Initial Commit

---
+++ UIImageViewGif/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIImageViewGif
+//
+//  Created by 尤艺琪 on 2018/11/21.
+//  Copyright © 2018 尤艺琪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ UIImageViewGifTests/UIImageViewGifTests.m
@@ -0,0 +1,37 @@
+//
+//  UIImageViewGifTests.m
+//  UIImageViewGifTests
+//
+//  Created by 尤艺琪 on 2018/11/21.
+//  Copyright © 2018 尤艺琪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UIImageViewGifTests : XCTestCase
+
+@end
+
+@implementation UIImageViewGifTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
